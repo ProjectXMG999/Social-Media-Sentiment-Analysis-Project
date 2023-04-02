1540f7a6382a7584d45418bd833388751e70ed97
Maciej Grabarczyk <97914504+ProjectXMG999@users.noreply.github.com>
2023-04-03 00:11:50 +0200

    Update Twitter_connection.py

--- Twitter_connection.py
+++ Twitter_connection.py
@@ -39,3 +39,36 @@ class TweetListener(tweepy.StreamListener):
 listener = TweetListener()
 stream = tweepy.Stream(auth=api.auth, listener=listener)
 stream.filter(track=['my_brand'])
+
+
+# analyze the data stored in the database to gain insights.
+# querying the database to find the most common named entities mentioned in tweets about our brand.
+
+import sqlite3
+
+# Connect to the SQLite database
+conn = sqlite3.connect('tweets.db')
+
+# Query the database to find the most common named entities
+results = conn.execute('''SELECT named_entities
+                          FROM tweets
+                          WHERE sentiment = 'positive' ''')
+
+# Count the frequency of named entities
+named_entities_count = {}
+for row in results:
+    named_entities = row[0].split(', ')
+    for named_entity in named_entities:
+        if named_entity in named_entities_count:
+            named_entities_count[named_entity] += 1
+        else:
+            named_entities_count[named_entity] = 1
+
+# Sort the named entities by frequency
+sorted_named_entities = sorted(named_entities_count.items(), key=lambda x: x[1], reverse=True)
+
+# Print the top 10 named entities
+for named_entity, count in sorted_named_entities[:10]:
+    print(named_entity, count)
+
+
